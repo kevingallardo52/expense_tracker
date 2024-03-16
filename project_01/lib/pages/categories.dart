f9cbfe54c18f0f4a55cf41288e3e24f19e0b3d46
Kevin Gallardo <94145042+kevingallardo52@users.noreply.github.com>
2024-03-16 09:39:20 -0400

    add settings and categories page

---
+++ project_01/lib/pages/categories.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import '../types/widgets.dart';
+
+class Categories extends WidgetWithTitle {
+  const Categories({super.key}) : super(title: "Add");
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Add");
+  }
+}
